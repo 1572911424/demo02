3a3a21818aec80a2f6e726a4e65134ecb2600c26
konglingjian <1572911424@qq.com>
2022-11-20 23:32:06 +0800

    five-commit

--- src/main/java/demo02/GitTest.java
+++ src/main/java/demo02/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master01");
         System.out.println("hot-fix-02");
+        System.out.println("test-push");
     }
 }
