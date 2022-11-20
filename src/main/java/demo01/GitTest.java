2c7fc50a519637a957255112f4f69ec33144706a
konglingjian <1572911424@qq.com>
2022-11-20 23:13:28 +0800

    second commit

--- src/main/java/demo01/GitTest.java
+++ src/main/java/demo01/GitTest.java
@@ -3,5 +3,6 @@ package demo01;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world01");
     }
 }
